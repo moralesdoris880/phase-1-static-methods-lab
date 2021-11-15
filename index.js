5955fae4d1d4437400f048956d664f445833a7d3
Doris Morales <moralesdoris880@gmail.com>
2021-11-15 18:13:33 -0500

    commit tree 970feec0dfc18e917dd1411cf40e8477c84c6b92

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string){
+    const nonalpha =  /[^A-Za-z0-9]/g;
+    let newstring =''
+    for(let char in string){
+      if (char === '-' || char === '/\'/g'|| char === '/\"/g'|| char === ' '){
+        newstring += char
+      }
+      else if (char === nonalpha){
+        newstring += ""
+      }
+    }
+    return newstring
+  }
+  static titleize(string){
+    return string.toUpperCase()
+  }
 }
